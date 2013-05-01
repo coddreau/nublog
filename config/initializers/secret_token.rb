c44cb66e42e5fbccf3baaa689cfb8d3adb3c9cc8
coddreau <coddreau@gmail.com>
2013-05-01 14:17:20 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nublog::Application.config.secret_key_base = '0fce67bb412f8dfe0991c6caaf8e8338bc8316917ac39541f5b84e31ff0c2ff9c1a6fe2f2a949c91c2d13b92c216638906641d11b2e6d422c0fcfb2007b2bf15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nublog::Application.config.session_store :cookie_store, key: '_nublog_session'
